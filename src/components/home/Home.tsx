c19016b475a95edc7c2a86f52300813f45320619
Luca Andrey <lucaandrey5@hotmail.com>
2022-09-28 12:19:37 -0300

    trocando css pro robo da plataforma validar

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,7 +2,7 @@ import "./Home.css";
 function Home() {
   return (
     <div>
-      <h1>HOME</h1>
+      <h1 className="titulo">HOME</h1>
       <img
         src="https://i.pinimg.com/originals/ba/87/cd/ba87cd3f4898cb1eb0b252964d4ae9cc.jpg"
         alt="galaxy image"
