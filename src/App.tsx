4170ab51194857f175d631168b853e52868de3a1
Luca Andrey <lucaandrey5@hotmail.com>
2022-09-30 23:56:09 -0300

    implementando funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          {" "}
+          // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
